f0601b82d2e359636b67ed42b8cf328d5e026758
user <123>
2021-02-01 15:07:33 +0800

    dict

---
+++ android2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+    private Button stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.btnstart);
+        stop = (Button) findViewById(R.id.btnstop);
+        //创建启动Service的Intent,以及Intent属性
+        final Intent intent = new Intent();
+        intent.setAction("com.example.myapplication.MyService");
+        //fix: Service Intent must be explicit, 需要将隐含意图转换为显示的意图，这里为什么要转换呢？？？
+        intent.setPackage("com.example.myapplication");
+        //为两个按钮设置点击事件,分别是启动与停止service
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+
+            }
+        });
+    }
+}
